bc719856f3c2dd62d27db74a008b29a4a4b2018d
sakthi_l_u_c_i <sakthivel8725@outlook.com>
2023-08-03 12:24:42 +0530

    feat: JobNest WebApp Initialized

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const plugin = require("tailwindcss/plugin");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          text: "#f1f3f2",
+          background: "#111311",
+          primary: "#786874",
+          secondary: "#1e1b1a",
+          accent: "#8a7a88",
+        },
+        light: {
+          text: "#111311",
+          background: "#f1f3f2",
+          primary: "#786874",
+          secondary: "#eae7e6",
+          accent: "#393238",
+        },
+        variants: {
+          extend: {
+            backgroundColor: ["dark", "dark-hover", "dark-group-hover"],
+            textColor: ["dark", "dark-hover", "dark-group-hover"],
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("tailwindcss-dark-mode")()],
+};
